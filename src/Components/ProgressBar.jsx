3d6f1b8aaaa29266922899d38d8a1b368a094888
Anthony Porthouse <anthony.porthouse@partnerize.com>
2021-02-20 15:20:25 +0000

    Add buffer bar to progress bar

--- src/Components/ProgressBar.jsx
+++ src/Components/ProgressBar.jsx
@@ -1,4 +1,4 @@
-import { useContext, useRef, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import Duration from "./Duration";
 import AudioContext from "./AudioContext";
 
@@ -13,15 +13,29 @@ function ProgressBar({ length, position }) {
   const [mousePercent, setMousePercent] = useState(0);
   const [mouseSongPos, setMouseSongPos] = useState(0);
 
+  const [bufferPercent, setBufferPercent] = useState(0);
+
+  useEffect(() => {
+    audio.addEventListener("progress", (e) => {
+      if (!audio.buffered?.length) {
+        return;
+      }
+      const buffered = audio.buffered.end(0);
+      const duration = audio.duration;
+
+      setBufferPercent((buffered / duration) * 100);
+    });
+  });
+
   const seek = (e) => {
     e.preventDefault();
-    audio.currentTime = mouseSongPos;
+    audio.fastSeek(mouseSongPos);
   };
 
   return (
     <div
       ref={progressBar}
-      className={`bg-gray-200 h-4 w-full inline-block`}
+      className={`bg-gray-200 h-4 w-full inline-block relative`}
       onMouseEnter={(e) => showPositionEnabled(true)}
       onMouseLeave={(e) => showPositionEnabled(false)}
       onMouseMove={(e) => {
@@ -38,14 +52,15 @@ function ProgressBar({ length, position }) {
       }}
       onClick={seek}
     >
+      <div
+        className={`h-full bg-gray-100 absolute`}
+        style={{ width: `${bufferPercent}%` }}
+      />
+
       {showPosition ? (
         <div
-          className={`h-full bg-green-200 relative`}
+          className={`absolute h-full bg-green-200`}
           style={{ width: `${mousePercent}%` }}
-          onClick={(e) => {
-            e.preventDefault();
-            // TODO: Jump to song position
-          }}
         >
           <div className={`absolute right-0 -mr-4 -mt-4 leading-none`}>
             <Duration time={mouseSongPos} />
@@ -53,7 +68,7 @@ function ProgressBar({ length, position }) {
         </div>
       ) : (
         <div
-          className={`h-full bg-green-200`}
+          className={`absolute h-full bg-green-200`}
           style={{ width: `${progress}%` }}
         />
       )}
