845229721b222bde9582fbf6459fa367304b2fad
Anthony Porthouse <anthony.porthouse@partnerize.com>
2021-02-20 01:37:28 +0000

    Begin implementing progress changing

--- src/Components/ProgressBar.jsx
+++ src/Components/ProgressBar.jsx
@@ -1,12 +1,53 @@
+import { useRef, useState } from "react";
+import Duration from "./Duration";
+
 function ProgressBar({ length, position }) {
   const progress = (position / length) * 100;
 
+  const progressBar = useRef(null);
+
+  const [showPosition, showPositionEnabled] = useState(false);
+  const [mousePercent, setMousePercent] = useState(0);
+  const [mouseSongPos, setMouseSongPos] = useState(0);
+
   return (
-    <div className={`bg-gray-200 h-4 w-full inline-block`}>
-      <div
-        className={`h-full bg-green-200`}
-        style={{ width: `${progress}%` }}
-      />
+    <div
+      ref={progressBar}
+      className={`bg-gray-200 h-4 w-full inline-block`}
+      onMouseEnter={(e) => showPositionEnabled(true)}
+      onMouseLeave={(e) => showPositionEnabled(false)}
+      onMouseMove={(e) => {
+        const bar = progressBar.current;
+        const barBounding = bar.getBoundingClientRect();
+
+        const position = e.pageX - barBounding.x;
+
+        const percentagePosition = position / barBounding.width;
+
+        const value = length * percentagePosition;
+        setMousePercent(percentagePosition * 100);
+        setMouseSongPos(value);
+      }}
+    >
+      {showPosition ? (
+        <div
+          className={`h-full bg-green-200 relative`}
+          style={{ width: `${mousePercent}%` }}
+          onClick={(e) => {
+            e.preventDefault();
+            // TODO: Jump to song position
+          }}
+        >
+          <div className={`absolute right-0 -mr-4 -mt-4 leading-none`}>
+            <Duration time={mouseSongPos} />
+          </div>
+        </div>
+      ) : (
+        <div
+          className={`h-full bg-green-200`}
+          style={{ width: `${progress}%` }}
+        />
+      )}
     </div>
   );
 }
