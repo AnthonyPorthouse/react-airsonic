7fa1b012ff743546f12a6e43879fd3c277731f0e
Anthony Porthouse <anthony.porthouse@partnerize.com>
2021-02-22 09:32:53 +0000

    Add ability to shuffle all tracks within an album

--- src/Components/AlbumHeader.jsx
+++ src/Components/AlbumHeader.jsx
@@ -11,6 +11,24 @@ function AlbumHeader({ album }) {
     dispatch(getNextTrack());
   };
 
+  const shuffleAll = (e) => {
+    e.preventDefault();
+
+    let tracks = album.tracks.slice();
+
+    const shuffle = (array) => {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * i);
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+
+      return array;
+    };
+
+    dispatch(setTracks(shuffle(tracks)));
+    dispatch(getNextTrack());
+  };
+
   return (
     <section className={`flex flex-col gap-6`}>
       <div className={`grid grid-cols-3 lg:grid-cols-1 gap-6 w-full lg:w-64`}>
@@ -21,6 +39,9 @@ function AlbumHeader({ album }) {
           <h3>{album.year}</h3>
           <button className={`w-full`} onClick={playAll}>
             Play All
+          </button>
+          <button className={`w-full`} onClick={shuffleAll}>
+            Shuffle All
           </button>
         </div>
       </div>
