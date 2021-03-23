7921544686e9f3e44b263e4c92134cb6ad79a402
Anthony Porthouse <anthony.porthouse@partnerize.com>
2021-03-23 14:36:58 +0000

    Added API call to search

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,25 +1,39 @@
 import { useState } from "react";
 import { ReactComponent as Search } from "../images/search.svg";
+import { useSelector } from "react-redux";
+import { selectAuth } from "../features/authSlice";
+import { getSearchResults } from "../features/api";
 
 function SearchBar() {
+  const auth = useSelector(selectAuth);
+
   const [filter, setFilter] = useState("");
 
+  const search = (query) => {
+    getSearchResults({ query, ...auth }).then((results) => {
+      console.log(results);
+    });
+  };
+
   return (
-    <form onSubmit={(event) => event.preventDefault()}>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        search(filter);
+      }}
+      autoComplete="off"
+    >
       <div className={"flex items-center border border-black p-0"}>
         <input
           className={"border-none focus:border-none px-3 py-2 pr-0"}
-          type="text"
+          autoComplete="off"
+          type="search"
           name="search"
           placeholder="Search"
           value={filter}
           onChange={(event) => setFilter(event.target.value)}
         />
-        <button
-          className={"px-3 py-2"}
-          onClick={(event) => event.preventDefault()}
-          aria-label={"Search"}
-        >
+        <button className={"px-3 py-2"} aria-label={"Search"}>
           <Search className={"h-6"} />
         </button>
       </div>
