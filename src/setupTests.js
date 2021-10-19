1fa0e3a926d9cba3069d91026cb5a6a836868de5
Anthony Porthouse <anthony@porthou.se>
2021-10-19 14:02:32 +0100

    Disable mocks server

--- src/setupTests.js
+++ src/setupTests.js
@@ -5,8 +5,8 @@
 import "@testing-library/jest-dom";
 import "jest-localstorage-mock";
 
-import { server } from "./mocks/server.js";
-
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
+// import { server } from "./mocks/server.js";
+//
+// beforeAll(() => server.listen());
+// afterEach(() => server.resetHandlers());
+// afterAll(() => server.close());
