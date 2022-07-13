a91823528e723e038ff6b05a88abfb14a203d06c
Anthony Porthouse <anthony@porthou.se>
2022-07-13 15:44:36 +0100

    Fix tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,17 +1,15 @@
-// import { render, screen } from "./testUtils";
-// import { MemoryRouter } from "react-router-dom";
-// import App from "./App";
-// import { waitFor } from "@testing-library/react";
+import { render, screen } from "./testUtils";
+import { MemoryRouter } from "react-router-dom";
+import App from "./App";
+import { waitFor } from "@testing-library/react";
 
-// test("Shows the log in form", async () => {
-//   render(<App />, { wrapper: MemoryRouter });
-//   const loginButton = screen.getByText(/log in/i);
-//   expect(loginButton).toBeInTheDocument();
-// });
+test("Shows the log in form", async () => {
+  render(<App />, { initialState: {}, wrapper: MemoryRouter });
+  const loginButton = screen.getByText(/log in/i);
+  expect(loginButton).toBeInTheDocument();
+});
 
-// test("Has the title set", async () => {
-//   render(<App />, { wrapper: MemoryRouter });
-//   await waitFor(() => expect(document.title).toBe("Ra"));
-// });
-
-export {};
+test("Has the title set", async () => {
+  render(<App />, { initialState: {}, wrapper: MemoryRouter });
+  await waitFor(() => expect(document.title).toBe("Ra"));
+});
