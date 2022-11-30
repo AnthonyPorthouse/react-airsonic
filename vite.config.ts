c8c70fcb5a5aa89cc50ba6400c618b8877573c7e
Anthony Porthouse <anthony@porthou.se>
2022-11-30 11:21:13 +0000

    format: Run prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgrPlugin from 'vite-plugin-svgr'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import svgrPlugin from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgrPlugin()],
   build: {
-    outDir: 'build'
-  }
-})
+    outDir: "build",
+  },
+});
