92371a39682c486474b7f561baa9e0f4006c0a47
Anthony Porthouse <anthony@porthou.se>
2022-12-01 11:25:31 +0000

    fix: tidy unneeded files

--- src/index.tsx
+++ src/index.tsx
@@ -37,9 +37,6 @@ if (import.meta.env.DEV) {
   setTimeout(() => document.dispatchEvent(new Event("update_available")), 5000);
 }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
 registerSW({
   onNeedRefresh: () => {
     document.dispatchEvent(new Event("update_available"));
@@ -49,4 +46,6 @@ registerSW({
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
+if (import.meta.env.DEV) {
+  reportWebVitals(console.log);
+}
