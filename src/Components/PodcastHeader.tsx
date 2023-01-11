abe72e6a53be74dfc2c41708373f1a4a09e63a8c
Anthony Porthouse <anthony@porthou.se>
2023-01-11 10:01:40 +0000

    Add podcast header

---
+++ src/Components/PodcastHeader.tsx
@@ -0,0 +1,29 @@
+import { SyntheticEvent } from "react";
+import { useTranslation } from "react-i18next";
+
+import type { Podcast } from "../api/podcasts";
+import { SongIds, Songs } from "../api/songs";
+import { useTrackList } from "../hooks";
+import AlbumArt from "./AlbumArt";
+
+interface PodcastHeaderProps {
+  podcast: Podcast;
+}
+
+function AlbumHeader({ podcast }: PodcastHeaderProps) {
+  const { t } = useTranslation("podcasts");
+
+  return (
+    <section className={`flex flex-col gap-6`}>
+      <div className={`grid grid-cols-3 lg:grid-cols-1 gap-6 w-full lg:w-64`}>
+        <AlbumArt id={podcast.coverArt} description={podcast.title} />
+        <div className={`col-span-2 lg:col-span-1`}>
+          <h1 className={`text-2xl lg:text-3xl`}>{podcast.title}</h1>
+          <p>{podcast.description}</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default AlbumHeader;
