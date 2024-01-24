2cb3d153bdeb78943b51edd4806d52650244f588
Anthony Porthouse <anthony@porthou.se>
2024-01-24 11:36:53 +0000

    fix(ui): correct bouncing when hovering over bottom of album

--- src/Components/Album.tsx
+++ src/Components/Album.tsx
@@ -12,16 +12,15 @@ function Album({ album, lazyLoad }: Readonly<AlbumProps>) {
   const { id, name, artist, coverArt } = album;
 
   return (
-    <Link
-      to={`/albums/${id}`}
-      className={`group block w-full relative rounded overflow-hidden hover:shadow-lg focus:-translate-y-2 hover:-translate-y-2 transition`}
-    >
-      <AlbumArt id={coverArt} description={name} lazyLoad={lazyLoad} />
-      <div
-        className={`absolute top-0 left-0 w-full h-full transition-all duration-200 ease-in-out bg-white/75 opacity-0 group-hover:opacity-100 group-focus:opacity-100 p-1 flex flex-col justify-center text-center`}
-      >
-        <h1 className={`text-xl`}>{name}</h1>
-        <h2>{artist}</h2>
+    <Link to={`/albums/${id}`} className={`group block w-full relative focus:ring-0`}>
+      <div className="group-focus:-translate-y-2 group-hover:-translate-y-2 group-focus:shadow-lg group-hover:shadow-lg overflow-hidden rounded transition group-focus:ring-inset">
+        <AlbumArt id={coverArt} description={name} lazyLoad={lazyLoad} />
+        <div
+          className={`absolute top-0 left-0 w-full h-full transition-all duration-200 ease-in-out bg-white/75 opacity-0 group-hover:opacity-100 group-focus:opacity-100 p-1 flex flex-col justify-center text-center`}
+        >
+          <h1 className={`text-xl`}>{name}</h1>
+          <h2>{artist}</h2>
+        </div>
       </div>
     </Link>
   );
