b296ae45c72475ebdf39389d3dac9c5a66181f3c
Anthony Porthouse <anthony@porthou.se>
2024-02-13 11:57:14 +0000

    style: apply prettier

--- src/Components/Duration.tsx
+++ src/Components/Duration.tsx
@@ -1,18 +1,23 @@
-import { Temporal } from 'temporal-polyfill'
+import { Temporal } from "temporal-polyfill";
 
 interface DurationProps {
   time: number;
 }
 
 function to2Digits(number: number) {
-  return String(number).padStart(2, '0')
+  return String(number).padStart(2, "0");
 }
 
 function Duration({ time }: Readonly<DurationProps>) {
+  const duration = Temporal.Duration.from({
+    milliseconds: Math.floor(time * 1000),
+  }).round({ largestUnit: "hour" });
 
-  const duration = Temporal.Duration.from({ milliseconds: Math.floor(time * 1000), }).round({ largestUnit: 'hour' });
-
-  const output = `${to2Digits(duration.hours)}:${to2Digits(duration.minutes)}:${to2Digits(duration.seconds)}`.replace('00:', '')
+  const output =
+    `${to2Digits(duration.hours)}:${to2Digits(duration.minutes)}:${to2Digits(duration.seconds)}`.replace(
+      "00:",
+      "",
+    );
 
   return <span>{output}</span>;
 }
