85f3072024476f1ca1406f8947f296bae8b79578
Anthony Porthouse <anthony@porthou.se>
2024-02-29 07:28:41 +0000

    build(ci): ensure coverage reports are sent to sonarcloud

--- vite.config.ts
+++ vite.config.ts
@@ -134,5 +134,9 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: "./src/test/setup.ts",
     css: true,
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html", "clover", "json", "lcov"],
+    },
   },
 });
