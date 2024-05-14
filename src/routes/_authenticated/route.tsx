1bbf57d658053803e077896101f93d4f0c1a50da
Anthony Porthouse <anthony@porthou.se>
2024-05-14 17:00:20 +0100

    fix: repopulate search query parameter on page refresh

--- src/routes/_authenticated/route.tsx
+++ src/routes/_authenticated/route.tsx
@@ -1,6 +1,11 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
+import { z } from "zod";
 
 export const Route = createFileRoute("/_authenticated")({
+  validateSearch: z.object({
+    query: z.string().optional().catch(""),
+  }),
+
   beforeLoad: ({ context, location }) => {
     if (!context.auth.isAuthenticated) {
       throw redirect({
