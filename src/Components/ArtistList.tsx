e40e414e398d337e2b9581f23e9dc98631e28a1c
Anthony Porthouse <anthony@porthou.se>
2024-06-10 11:40:46 +0100

    feat: group albums by starting letter

--- src/Components/ArtistList.tsx
+++ src/Components/ArtistList.tsx
@@ -1,3 +1,5 @@
+import { Fragment, useMemo } from "react";
+
 import { Artists } from "../api/artists.js";
 import ArtistItem from "./ArtistItem.js";
 import Grid from "./Grid.js";
@@ -8,13 +10,43 @@ interface ArtistListProps {
 }
 
 function ArtistList({ className, artists }: Readonly<ArtistListProps>) {
-  return (
-    <Grid className={className}>
-      {artists.map((artist) => (
-        <ArtistItem key={artist.id} artist={artist} />
-      ))}
-    </Grid>
+  const artistsByLetter = useMemo(() => {
+    return artists.reduce((prev: { [key: string]: Artists }, artist) => {
+      let key = artist.name.charAt(0).toUpperCase();
+
+      if (Number.isInteger(key)) {
+        key = "#";
+      }
+
+      if (!prev[key]) {
+        prev[key] = [];
+      }
+
+      prev[key] = [...prev[key], artist];
+
+      return prev;
+    }, {});
+  }, [artists]);
+
+  const sortedKeys = useMemo(
+    () => Object.keys(artistsByLetter).sort(),
+    [artistsByLetter],
   );
+
+  return sortedKeys.map((key) => {
+    const artists = artistsByLetter[key];
+
+    return (
+      <Fragment key={key}>
+        <h1 className={`my-4 mb-1 text-2xl`}>{key}</h1>
+        <Grid className={className}>
+          {artists.map((artist) => (
+            <ArtistItem key={artist.id} artist={artist} />
+          ))}
+        </Grid>
+      </Fragment>
+    );
+  });
 }
 
 export default ArtistList;
