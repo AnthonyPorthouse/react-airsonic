7153ce666a9dc5edfed11ae03d07aae786144866
Anthony Porthouse <anthony@porthou.se>
2024-07-27 21:16:00 +0100

    chore: remove commented out config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,3 @@
-// module.exports = {
-//   env: {
-//     browser: true,
-//     es2021: true,
-//   },
-//   extends: [
-//     "eslint:recommended",
-//     "plugin:@typescript-eslint/recommended",
-//     "plugin:react-hooks/recommended",
-//     "plugin:@tanstack/eslint-plugin-query/recommended",
-//     "prettier",
-//   ],
-//   overrides: [
-//     {
-//       env: {
-//         node: true,
-//       },
-//       files: [".eslintrc.{js,cjs}"],
-//       parserOptions: {
-//         sourceType: "script",
-//       },
-//     },
-//   ],
-//   parser: "@typescript-eslint/parser",
-//   parserOptions: {
-//     ecmaVersion: "latest",
-//     sourceType: "module",
-//   },
-//   plugins: ["react-refresh"],
-//   rules: {},
-//   ignorePatterns: ["**/*.gen.ts", "build", ".eslintrc.cjs", "*.config.js"],
-// };
 // @ts-check
 import eslint from "@eslint/js";
 import tanstackQuery from "@tanstack/eslint-plugin-query";
