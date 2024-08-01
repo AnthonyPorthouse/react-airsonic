ba0bea44f52b8cc5477c8ec727c05faad3dbefc6
Anthony Porthouse <anthony@porthou.se>
2024-08-01 10:59:06 +0100

    test: add album provider tests
    refator: use the renderHook method in the podcast provider test

---
+++ src/Providers/AlbumProvider.test.tsx
@@ -0,0 +1,35 @@
+import { Song } from "@/api/types";
+import { renderHook } from "@testing-library/react";
+
+import { AlbumProvider, useAlbumTracks } from "./AlbumProvider";
+
+const testSong: Song = {
+  id: "abc",
+  title: "Test Track",
+  track: 1,
+  discNumber: 1,
+  album: "Test Album",
+  albumId: "123",
+  artist: "Test Artist",
+  artistId: "xyz",
+  coverArt: "123456",
+  duration: 60,
+  isPodcast: false,
+  parent: "",
+};
+
+describe(AlbumProvider, async () => {
+  it("should return an empty array on no tracks", async () => {
+    const { result } = renderHook(() => useAlbumTracks());
+    expect(result.current).toEqual([]);
+  });
+
+  it("should return the tracks if there are any", async () => {
+    const { result } = renderHook(() => useAlbumTracks(), {
+      wrapper: ({ children }) => (
+        <AlbumProvider tracks={[testSong]}>{children}</AlbumProvider>
+      ),
+    });
+    expect(result.current).toEqual([testSong]);
+  });
+});
--- src/Providers/PodcastProvider.test.tsx
+++ src/Providers/PodcastProvider.test.tsx
@@ -1,21 +1,7 @@
 import { Episode } from "@/api/types";
-import { render, screen } from "@testing-library/react";
+import { renderHook } from "@testing-library/react";
 
 import { PodcastProvider, useEpisodes } from "./PodcastProvider";
-
-function TestComponent() {
-  const episodes = useEpisodes();
-
-  return (
-    <ul>
-      {episodes.map((episode) => (
-        <li data-testid={episode.id} key={episode.id}>
-          {episode.id} {episode.status} {episode.title}
-        </li>
-      ))}
-    </ul>
-  );
-}
 
 const testEpisode: Episode = {
   id: "s-1",
@@ -28,33 +14,20 @@ const testEpisode: Episode = {
 describe(PodcastProvider, async () => {
   describe("with no episodes", async () => {
     it("renders an empty list", async () => {
-      render(
-        <PodcastProvider episodes={[]}>
-          <TestComponent />
-        </PodcastProvider>,
-      );
-
-      const list = await screen.findByRole("list");
-
-      expect(list).toBeEmptyDOMElement();
+      const { result } = renderHook(() => useEpisodes());
+      expect(result.current).toEqual([]);
     });
   });
 
   describe("with episodes", async () => {
     it("returns the episodes", async () => {
-      render(
-        <PodcastProvider episodes={[testEpisode]}>
-          <TestComponent />
-        </PodcastProvider>,
-      );
+      const { result } = renderHook(() => useEpisodes(), {
+        wrapper: ({ children }) => (
+          <PodcastProvider episodes={[testEpisode]}>{children}</PodcastProvider>
+        ),
+      });
 
-      const list = await screen.findByRole("list");
-
-      expect(list).not.toBeEmptyDOMElement();
-
-      const item = await screen.findByRole("listitem");
-
-      expect(item).toHaveTextContent("s-1 skipped test");
+      expect(result.current).toEqual([testEpisode]);
     });
   });
 });
