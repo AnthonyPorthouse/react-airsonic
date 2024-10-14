a2528693a289b27269d664dbd5b3e8d4753bfc1a
Anthony Porthouse <anthony@porthou.se>
2024-10-14 19:27:10 +0100

    build: update dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,37 @@
-// @ts-check
 import eslint from "@eslint/js";
 import tanstackQuery from "@tanstack/eslint-plugin-query";
 import prettier from "eslint-config-prettier";
-import reacthooks from "eslint-plugin-react-hooks";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   {
     ignores: ["build/**", "*.config.*"],
   },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
   {
-    plugins: {
-      "react-hooks": reacthooks,
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tanstackQuery.configs["flat/recommended"],
+    ],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      globals: globals.browser,
     },
-
-    rules: reacthooks.configs.recommended.rules,
-  },
-  ...tanstackQuery.configs["flat/recommended"],
-  {
-    rules: prettier.rules,
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      ...prettier.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
   },
 );
--- src/routes/_authenticated/now-playing.lazy.tsx
+++ src/routes/_authenticated/now-playing.lazy.tsx
@@ -11,5 +11,13 @@ export const Route = createLazyFileRoute("/_authenticated/now-playing")({
 function NowPlaying() {
   const { trackList } = useTrackList();
 
+  if (trackList.length === 0) {
+    return (
+      <div>
+        <h1 className="text-2xl font-bold">Nothing Currently Playing</h1>
+      </div>
+    );
+  }
+
   return <TrackList tracks={trackList} />;
 }
