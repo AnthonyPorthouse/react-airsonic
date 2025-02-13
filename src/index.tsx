988e61a558573b4e95837ca7398bdcdb27be6d5f
Anthony Porthouse <anthony@porthou.se>
2025-02-13 10:57:46 +0000

    build: add sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { AuthProvider } from "@providers/AuthProvider.js";
 import { TrackListProvider } from "@providers/TrackListProvider.js";
+import Sentry from "@sentry/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
@@ -10,6 +11,21 @@ import ReactModal from "react-modal";
 import App from "./App.js";
 import "./i18n";
 import "./index.css";
+
+Sentry.init({
+  dsn: "https://7f604bbfd4d89d353f3fa13222854efc@sentry.porthouse.dev/2",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/ra.porthou.se/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 const container = document.getElementById("root") as HTMLElement;
 
