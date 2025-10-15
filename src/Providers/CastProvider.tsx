e30cbebe9039d7255a00e693cc4a357815aa39be
Anthony Porthouse <anthony.porthouse@mhr.co.uk>
2025-10-15 08:47:54 +0100

    feat: extend Chromecast integration and update dependencies
    - Added `@types/chrome` and `@types/chromecast-caf-sender` for better type support.
    - Updated `useCast` and `CastProvider` to use `window.cast` and `window.chrome` directly.
    - Included new type definitions in `tsconfig.json`.

--- src/Providers/CastProvider.tsx
+++ src/Providers/CastProvider.tsx
@@ -1,29 +1,30 @@
-import { PropsWithChildren, useEffect, useState } from "react";
 import { CastContext } from "@/Contexts/CastContext.ts";
+import { PropsWithChildren, useEffect, useState } from "react";
 
-function CastProvider({children}: Readonly<PropsWithChildren>) {
-  
-  const [loaded, setLoaded] = useState(false)
+function CastProvider({ children }: Readonly<PropsWithChildren>) {
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
-    // @ts-expect-error @typescript-eslint/no-unused-vars
-    window["__onGCastApiAvailable"] = (isAvailable: boolean) => {
-      const instance = window['cast'].framework.CastContext.getInstance();
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    window.__onGCastApiAvailable = (isAvailable: boolean) => {
+      const instance = window.cast.framework.CastContext.getInstance();
       instance.setOptions({
-        receiverApplicationId: window['chrome'].cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID,
-        autoJoinPolicy: window['chrome'].cast.AutoJoinPolicy.ORIGIN_SCOPED,
+        receiverApplicationId:
+          window.chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID,
+        autoJoinPolicy: window.chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED,
       });
 
-      console.log(instance)
-    }
+      console.log(instance);
+    };
 
     const scriptTag = document.createElement("script");
-    scriptTag.src = "https://www.gstatic.com/cv/js/sender/v1/cast_sender.js?loadCastFramework=1";
+    scriptTag.src =
+      "https://www.gstatic.com/cv/js/sender/v1/cast_sender.js?loadCastFramework=1";
     scriptTag.async = true;
     scriptTag.onload = () => setLoaded(true);
     document.body.appendChild(scriptTag);
   }, []);
-  
+
   return <CastContext.Provider value={loaded}>{children}</CastContext.Provider>;
 }
 
--- src/hooks/useCast.ts
+++ src/hooks/useCast.ts
@@ -1,13 +1,12 @@
-import { useContext } from "react";
 import { CastContext } from "@/Contexts/CastContext.ts";
+import { useContext } from "react";
 
 export function useCast() {
   const loaded = useContext<boolean>(CastContext);
 
   return {
     loaded,
-    cast: window['cast'] as unknown,
-    chrome: window['chrome'] as unknown,
-  }
-
+    cast: window.cast,
+    chrome: window.chrome,
+  };
 }
